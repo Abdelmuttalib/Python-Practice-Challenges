d2b0673d7c2f16242ee96da5b54552ac9fd3f6a9
Abdelmuttalib Ahmed <hagar9886@gmail.com>
2020-12-27 09:33:30 +0800

    Update timeConversion.py

--- Time Conversion Challenge/timeConversion.py
+++ Time Conversion Challenge/timeConversion.py
@@ -45,45 +45,29 @@ Sample Output 0
 
 def timeConversion(s):
 	## Splitting the time string to find the time label
-	switched = s.split(':')
-
-	## initialize array to hold the final changed string format
-	fixed_time = s[:-2].split(':')
-	## Cutting the time label (AM or PM) from the string
-	cut_time_label = switched[len(switched) - 1]
-	day_time_label = cut_time_label[2:]
-	## to check the first number (hour) and change it accordingly to 24-hour format
-	time_switcher = fixed_time[0]
-	## array of possible values after 12PM, to check which matched the inputed hour and change it to 24-hour format
-	check_array = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11']
+	splited_time = s.split(':')
+	## holding the time label (AM or PM) in variable to determine day time and convert to 24-hour format properly
+	day_time_label = splited_time[2][2:]
 
 	## if the day time label is AM, then the only case we need to change the hour format is at 12:00:00 AM to 00:00:00
-	if day_time_label == 'AM' and time_switcher == '12':
-		time_switcher = '00'
+	## also remove the day_time_label (AM) from the string
+	if day_time_label == 'AM' and splited_time[0] == '12':
+		splited_time[0] = '00'
+		splited_time[2] = splited_time[2].replace("AM", "")
+
 	## if day time label is PM then change hour format accordingly by adding 12 to it
+	## and remove the day_time_label (PM) from the string
 	elif day_time_label == 'PM':
-		for j in check_array:
-			if time_switcher == j:
-				## change value to integer to perform math addition then return again as string
-				time_switcher = str(int(j) + 12)
-	## assigning the value of the fixed hour format to the array of time (divided into: hour, min, sec),
-	## we are assigning only the correct hour format after fixing it to 24-hour format,
-	## as the Minutes & Seconds are not different in 24-hour format
-	fixed_time[0] = time_switcher
+		if splited_time[0] != '12':
+			splited_time[0] = str(int(splited_time[0]) + 12)
+			splited_time[2] = splited_time[2].replace("PM", "")
 
-	## final time string after converting to 24-hour format
-	final_time_shape = ""
-	## iterate over the array of converted (hour, min, sec) and adding it as string to 'final_time_shape'
-	for i in range(len(fixed_time)):
-		## when selected value is the last  (second), do not add ':'
-		if i == len(fixed_time) - 1:
-			final_time_shape += fixed_time[i]
-		else:
-			## only add ':' to seperate hour, min & sec if the selected value isn't the seconds
-			final_time_shape += fixed_time[i] + ":"
+	## final converted time to 24-hour format
+	## join the list to string seperated by ':'
+	final_converted_time = ":".join(splited_time)
 
-	## Returning the final time result converted in 24-hour format
-	return final_time_shape
+	## returning the final result
+	return final_converted_time
 
 
 
